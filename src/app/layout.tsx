b4fa8a633da180f0adf623988a43f7d6940d2da2
Tristan <heytristaann@gmail.com>
2023-12-12 15:41:20 -0500

    cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
 
 const poppins = Poppins({
   weight: ["300", "400", "500", "600", "700", "800", "900"],
